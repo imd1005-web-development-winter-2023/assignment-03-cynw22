30b0dc69ab5f27825da2a05941b8fd99735ddfae
cynthia wang <cynthia.wang218@gmail.com>
2023-03-25 02:30:33 -0400

    added clear all

--- main.js
+++ main.js
@@ -62,6 +62,10 @@ function switchIndex() {
   console.log("todos= ", todos);
 }
 
+let buttonsArray = [];
+
+
+////////////////////////////////////////////////////////////////////////////////////////
 // const todonelist = document.querySelector(".todonelist1");
 // // const todos = ["chips"];
 
@@ -104,95 +108,165 @@ remake whole todo funciton
 
 */
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////
 let todos = [];
 
 let doneTodos = [];
 
+let numOfButtons = 0;
+let liArray = [];
+let doneArray = [];
+let tempText = "";
+let isDone = [];
+
 function drawToDoList() {
-  // Clear all of the entries in the list
-  while (todoList.firstChild) {
-    todoList.removeChild(todoList.firstChild);
+  // // Clear all of the entries in the list
+  // while (todoList.firstChild) {
+  //   todoList.removeChild(todoList.firstChild);
+  // }
+
+  // for (let i = 0; i < todos.length; i++) {
+  //   const listItem = document.createElement("li");
+  //   //listItem.textContent = todos[i].text;
+  //   const taskText = document.createElement("span");
+  //   taskText.textContent = todos[i].text;
+
+  //   if (todos[i].isDone === true) {
+  //     listItem.classList.add("done");
+  //   }
+
+  //   const todoDeleteButton = document.createElement("button");
+  //   todoDeleteButton.textContent = "Delete";
+  //   todoDeleteButton.classList.add("todoDeleteButton");
+
+  //   todoDeleteButton.dataset.index = i;
+
+  //   todoDeleteButton.addEventListener("click", deleteTodo);
+
+  //   const todoDoneButton = document.createElement("button");
+
+  //   todoDoneButton.classList.add("doneButton");
+
+  //   if (todos[i].isDone === true) {
+  //     todoDoneButton.textContent = "UnDone";
+  //     //push one element off and then splice command similar to delete to new list
+  //     //arr.splice(new, 1replace, old);
+
+  //   } else {
+  //     todoDoneButton.textContent = "Done";
+  //   }
+
+  //   todoDoneButton.dataset.index = i;
+
+  //   todoDoneButton.addEventListener("click", doneTodo);
+
+  //   listItem.appendChild(todoDoneButton);
+  //   listItem.appendChild(todoDeleteButton);
+  //   listItem.appendChild(taskText);
+
+  //   todoList.appendChild(listItem);
+  // }
+
+  //remaking entire button is clicked sequence
+  console.log("button is clicked");
+  liArray[numOfButtons] = document.createElement("li");
+  
+  //delete button
+  const todoDeleteButton = document.createElement("button");
+  todoDeleteButton.textContent = "Delete";
+  todoDeleteButton.classList.add("todoDeleteButton");
+  todoDeleteButton.setAttribute("onclick", "deleteClick("+numOfButtons+")"); 
+
+  //done button
+  doneArray[numOfButtons] = document.createElement("button");
+  doneArray[numOfButtons].classList.add("doneButton");
+  doneArray[numOfButtons].textContent = "Done";
+  doneArray[numOfButtons].setAttribute("onclick", "doneClick("+numOfButtons+")"); 
+  isDone[numOfButtons] = 1;
+
+  //text
+  const taskText = document.createElement("span");
+  taskText.textContent = tempText;
+  
+  //appending
+  todoList.appendChild(liArray[numOfButtons]);
+  liArray[numOfButtons].appendChild(todoDeleteButton);
+  liArray[numOfButtons].appendChild(doneArray[numOfButtons]);
+  liArray[numOfButtons].appendChild(taskText);
+  
+  numOfButtons++;
+}
+
+function deleteClick(buttonIndex){
+  console.log("delete click: ", buttonIndex);
+  todoList.removeChild(liArray[buttonIndex]);
+}
+
+function doneClick(buttonIndex){
+  console.log("done click: ", buttonIndex);
+  if (isDone[buttonIndex] == 1) {
+    //either one works
+    liArray[buttonIndex].classList.add("done");
+    //doneArray[buttonIndex].classList.add("done");
+    doneArray[buttonIndex].textContent = "UnDone";
+    isDone[buttonIndex] *= -1;
   }
-
-  for (let i = 0; i < todos.length; i++) {
-    const listItem = document.createElement("li");
-    //listItem.textContent = todos[i].text;
-    const taskText = document.createElement("span");
-    taskText.textContent = todos[i].text;
-
-    if (todos[i].isDone === true) {
-      listItem.classList.add("done");
-    }
-
-    const todoDeleteButton = document.createElement("button");
-    todoDeleteButton.textContent = "Delete";
-    todoDeleteButton.classList.add("todoDeleteButton");
-
-    todoDeleteButton.dataset.index = i;
-
-    todoDeleteButton.addEventListener("click", deleteTodo);
-
-    const todoDoneButton = document.createElement("button");
-
-    todoDoneButton.classList.add("doneButton");
-
-    if (todos[i].isDone === true) {
-      todoDoneButton.textContent = "UnDone";
-      //push one element off and then splice command similar to delete to new list
-      //arr.splice(doneTodos[i], 1, todos[i]);
-
-    } else {
-      todoDoneButton.textContent = "Done";
-    }
-
-    todoDoneButton.dataset.index = i;
-
-    todoDoneButton.addEventListener("click", doneTodo);
-
-    listItem.appendChild(todoDoneButton);
-    listItem.appendChild(todoDeleteButton);
-    listItem.appendChild(taskText);
-
-    todoList.appendChild(listItem);
+  else{
+    liArray[buttonIndex].classList.remove("done");
+    //doneArray[buttonIndex].classList.remove("done");
+    doneArray[buttonIndex].textContent = "Done";
+    isDone[buttonIndex] *= -1;
   }
 }
 
-function doneTodo(event) {
-  console.log("Marked as done");
+function clearAll()
+{
+  console.log("liArray", liArray);
 
-  todoDeleteIndex = event.target.dataset.index;
-
-  console.log("INDEX: ", todoDeleteIndex);
-
-  todos[todoDeleteIndex].isDone = !todos[todoDeleteIndex].isDone;
-
-  console.log(todos);
-
-  drawToDoList();
+  for (let i=0; liArray.length > i; i++ )
+  {
+    deleteClick(i);
+    numOfButtons = 0;
+  }
+  console.log("clear all");
 }
 
-function deleteTodo(event) {
-  console.log("Delete button index", event.target.dataset.index);
+///////////////////////////////////////////////////////////////////////////////
+// function doneTodo(event) {
+//   console.log("Marked as done");
 
-  todoDeleteIndex = event.target.dataset.index;
+//   todoDeleteIndex = event.target.dataset.index;
 
-  todos.splice(todoDeleteIndex, 1);
+//   console.log("INDEX: ", todoDeleteIndex);
 
-  drawToDoList();
-}
+//   todos[todoDeleteIndex].isDone = !todos[todoDeleteIndex].isDone;
+
+//   console.log(todos);
+
+//   drawToDoList();
+// }
+
+// function deleteTodo(event) {
+//   console.log("Delete button index", event.target.dataset.index);
+
+//   todoDeleteIndex = event.target.dataset.index;
+
+//   todos.splice(todoDeleteIndex, 1);
+
+//   drawToDoList();
+// }
 
 function addTodo(event) {
   event.preventDefault();
 
-  todoTextFromForm.value;
+  tempText = todoTextFromForm.value;
 
   // todos.push(todoTextFromForm.value);
 
-  todos.push({
-    text: todoTextFromForm.value,
-    isDone: false,
-  });
+  // todos.push({
+  //   text: todoTextFromForm.value,
+  //   isDone: false,
+  // });
 
   console.log(todos);
 
@@ -202,6 +276,8 @@ function addTodo(event) {
 }
 
 form.addEventListener("submit", addTodo);
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 //  You may remove the code below - it's just boilerplate
 //
